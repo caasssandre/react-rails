612f2c2bd3f4e80386b925145f71675008569368
Cassandre-Guinut <guinutcassandre@gmail.com>
2020-01-22 19:15:47 +1300

    add react router

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Home extends React.Component {
+  render(){
+    return(
+      <>
+        <h1>Home</h1>
+        <a href='/contact'>Contact</a>
+      </>
+    )
+  }
+}
+
+export default Home
